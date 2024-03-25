558ae54e88589d99c0c4e5e933f44b0df07f6269
ferloayza <fer.loayza.caceres@gmail.com>
2024-03-24 21:26:21 -0500

    24/03 update

--- Mix design.py
+++ Mix design.py
@@ -5,7 +5,7 @@ Created on Thu Jan  5 10:22:22 2023
 @author: ferna
 """
 
-# Importacion de Librerias
+# Importing necessary libraries
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,13 +18,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 
-# Importacion del conjunto de datos
-dataset = pd.read_csv('C:/Users/ferna/OneDrive/Documentos/Data science/Concrete Mix Design with Neural Network/Database/Database.csv')
+# Importing the dataset
+dataset = pd.read_csv('C:/Users/ferna/OneDrive/Documentos/Data science/Concrete Mix Design with Neural Network/Database/MX_Database.csv')
 
 X = dataset.iloc[:, :9].values
 y = dataset.iloc[:,9:13].values
 
-# Codificacion de datos categoricos
+# Data Preprocessing
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
@@ -37,68 +37,56 @@ ss_y= MinMaxScaler()
 X = ss_X.fit_transform(X)
 y = ss_y.fit_transform(y)
 
-def plot_X(X):
-    plt.figure(figsize=(10,6))
-    sns.boxplot(data=X)
-    plt.xticks(rotation=90);
-    
-    plot_X(X)
 
-# Division de Los datos en datos de entrenamiento y datos de prueba
+# Splitting the train and test data
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size = 0.2,
                                                     random_state = 1)
 
 
-# Parte 2 - Creacion de la red neuronal!
-# Importacion de Librerias y paquetes de keras
+# Part 2. Neural Network
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-#Inicializamos la Red Neuronal
+
 red = Sequential ()
-#Creamos La capa de entrada
+#Input Layer
 red.add(Dense(
       9,
-      input_dim = 9,
       activation = 'sigmoid'
       ))
-#ler Capa oculta
+#Hidden Layer
 red.add(Dense(
         14,
         activation = 'sigmoid'
         ))
-#Capa de salida
+#Output Layer
 red.add(Dense(
         4,
         activation= 'linear'
         ))
 
-initial_learning_rate = 0.001
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate,
-    decay_steps=1000,
-    decay_rate=0.96,
-    staircase=True)
+
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 red.compile(
             optimizer=optimizer,
             loss = 'mse',
             metrics = ['mae','mean_squared_logarithmic_error']
             )
-#Entrenamiento
+#Training
 history = red. fit(X_train, y_train,
                    epochs = 1000 ,
                    batch_size= 45,
                    verbose=1,
                    validation_data=(X_test,y_test))
-# Prediccion
-diseño = pd.read_csv('C:/Users/ferna/OneDrive/Documentos/Data science/Concrete Mix Design with Neural Network/Input/Input.csv')
+# Prediction
+diseño = pd.read_csv('C:/Users/ferna/OneDrive/Documentos/Data science/Concrete Mix Design with Neural Network/Input data/MX_Input.csv')
 Z = diseño.iloc[:, 0:9].values
 
-seco=ss_y.inverse_transform(red.predict(ss_X.transform(np.array(Z))))
+output=ss_y.inverse_transform(red.predict(ss_X.transform(np.array(Z))))
 
 
 def plot_history(history):
